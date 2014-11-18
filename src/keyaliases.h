068c2ed4a1dc1ab0f87ec9efb820b9fec422606b
Bradley Garagan <bradleygaragan@live.ca>
2014-11-18 15:49:38 -0400

    refactor key macros - they now reside in their own header

---
+++ src/keyaliases.h
@@ -0,0 +1,23 @@
+#ifndef __KEYALIASES_H__
+#define __KEYALIASES_H__ value
+
+#define KEY_MOVE_LEFT  'h'
+#define KEY_MOVE_RIGHT 'l'
+#define KEY_MOVE_UP    'k'
+#define KEY_MOVE_DOWN  'j'
+
+#define C_RIGHT        555
+#define C_LEFT         540
+
+#define KEY_INSERT     'i'
+#define KEY_APPEND     'a'
+
+#define KEY_NL_BELOW   'o'
+#define KEY_NL_ABOVE   'O'
+
+#define KEY_APPEND_EOL 'A'
+#define KEY_INSERT_SOL 'I'
+
+#define KEY_COM_MODE   ';'
+
+#endif
